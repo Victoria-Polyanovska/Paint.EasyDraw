cfefb794a1976ccb5e237efec38fa7a43852f452
Victoria-Polyanovska <ipz242_pve@student.ztu.edu.ua>
2026-04-26 19:16:48 +0300

    ініціалізація полотна

--- Form1.cs
+++ Form1.cs
@@ -5,7 +5,29 @@ namespace paint
         public Form1()
         {
             InitializeComponent();
+
+            this.Width = 1330;
+            this.Height = 750;
+
+            bm = new Bitmap(pic.Width, pic.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            pic.Image = bm;
+
+            trackBar1.Value = 5;
+            erase.Width = trackBar1.Value;
+
         }
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Point px, py;
+        Pen p = new Pen(Color.Black, 1);
+        Pen erase = new Pen(Color.White, 10);
+        int index;
+
+        ColorDialog cd = new ColorDialog();
+        Color new_color;
 
         private void pic_Click(object sender, EventArgs e)
         {
@@ -25,6 +47,13 @@ namespace paint
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            StartForm startForm = new StartForm();
+            startForm.Show();
+            this.Close();
         }
     }
 }
